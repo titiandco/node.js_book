658ff800a5809ddbd25bae26f9ec54cf6724f340
titiandco <placidolopez123@hotmail.com>
2016-11-04 20:53:22 +0100

    rectified

--- test.js
+++ test.js
@@ -3,8 +3,8 @@
    -1 node_modules
    -2 own's module created
    -3 external module from other developer*/
-var mod1 = require ("./module1.js"); /* this is a own module
-									the path begin by ./     */
+var mod1 = require ("./module1.js"); 
+
 
 console.log(mod1);
 
