c09b31c9adbac21900367eb4b0c5dd1c6e54cdc0
titiandco <placidolopez123@hotmail.com>
2016-11-04 20:32:59 +0100

    correction words kind
    zz
    
    	modified:   module1.js

---
+++ module1.js
@@ -0,0 +1,1 @@
+console.log("File module1.js has been loaded")
--- test.js
+++ test.js
@@ -1,8 +1,10 @@
 
-/* there is 3 kings  of module's require 
+/* there is 3 kinds  of module's require 
    -1 node_modules
    -2 own's module created
    -3 external module from other developer*/
-var mod1 = require ("./module1.js"); /* this is a own module*/
+var mod1 = require ("./module1.js"); /* this is a own module
+									the path begin by ./     */
 
 console.log(mod1);
+
