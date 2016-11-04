96371a7de5a28fe2f0ada7e89609c4cb00f048d5
titiandco <placidolopez123@hotmail.com>
2016-11-04 20:29:19 +0100

    practise

---
+++ test.js
@@ -0,0 +1,8 @@
+
+/* there is 3 kings  of module's require 
+   -1 node_modules
+   -2 own's module created
+   -3 external module from other developer*/
+var mod1 = require ("./module1.js"); /* this is a own module*/
+
+console.log(mod1);
